40c5f317773c778d56b0c6e250a7c5a531110a8e
Michael Wall <mikesterwallace8@gmail.com>
2020-12-27 11:24:55 -0500

    cocktail search script by name working

---
+++ script.js
@@ -0,0 +1,32 @@
+
+ document.getElementById("cocktailBtn").addEventListener("click", function() {
+    let searchField = document.getElementById("cocktailSearch");
+
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + searchField.value)
+    .then((response) => {
+        return response.json();
+    })
+    // ************************************************** APP  starts HERE*****************************************************
+    .then((cocktailData) => {
+    
+        for (let i =0 ; i < cocktailData.drinks.length ; i++) {
+            let displayCocktail = document.createElement("div");
+            let cocktailImage = document.createElement("img");
+    
+            displayCocktail.innerHTML = cocktailData.drinks[i].strDrink;
+            cocktailImage.setAttribute("src", cocktailData.drinks[i].strDrinkThumb)
+
+            let displayDrinks = document.getElementById("cocktailResults")
+
+            displayDrinks.appendChild(displayCocktail)
+            displayDrinks.appendChild(cocktailImage)
+
+        }
+
+
+
+
+        console.log(cocktailData)
+    });
+});   
+
