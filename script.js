6e64373c8a9bc391de80bc0b7a6758315435d0fb
Kirsi Ralko <kirsiralko@gmail.com>
2020-12-30 15:42:56 -0600

    newly styled index page, created separate style.css

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// Countdown line on main page
+var countDownDate = new Date("May 5, 2021 00:00:01").getTime();
 
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+// Display the result in the element with id="demo", update every second
+document.getElementById("countdown").innerHTML = "<b>" + days + " Days until Cinco de Mayo 2021!" + "</b>";
+}, 1000);
+
+
+// Retrieve images of cocktail results
  document.getElementById("cocktailBtn").addEventListener("click", function() {
     let searchField = document.getElementById("cocktailSearch");
 
